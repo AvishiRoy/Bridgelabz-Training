c47c19d2c06650c3108f3f41a22476adbe4a99c2
Avishi Roy <avishi.feb28@gmail.com>
2026-03-05 19:47:50 +0530

    UC4: Stack palindrome implementation

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
@@ -37,6 +39,23 @@ public class PalindromeCheckerApp {
         }
 
         return true;
+    }
+
+    public static boolean stackPalindrome(String word){
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : word.toCharArray()){
+            stack.push(c);
+        }
+
+        String reversed = "";
+
+        while(!stack.isEmpty()){
+            reversed += stack.pop();
+        }
+
+        return word.equals(reversed);
     }
 
 }
